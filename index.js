2588026c930fdba7881131385dde36a9bba02e8f
JBRBeans <juliearitola1@gmail.com>
2024-10-23 10:28:34 -0700

    fixed repo, worked on responses

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Initialize dotenv
 require('dotenv').config();
-<<<<<<< HEAD
 
-=======
->>>>>>> 1bea2f7dcb77a9662fddc05d7c174d6bb8f47cb8
 // Discord.js versions ^13.0 require us to explicitly define client intents
 const { Client, GatewayIntentBits, Attachment, AttachmentBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
@@ -33,12 +30,9 @@ const response1 = "*runs to you* meoooow :3";
 const response2 = "*Pukes on the carpet cutely* :3c mrrt"; // thank you seika
 const response3 = "*takes a bite when you're not looking*";
 
-<<<<<<< HEAD
 const response4 = "*jumps around to catch it* n<(^w^)>n" + '\n' + "*prrt* :3c *catches light*";
-=======
 const response4a = "*jumps around to catch it* n<(^w^)>n";
 const response4b = "*prrt* :3c *catches light*";
->>>>>>> 1bea2f7dcb77a9662fddc05d7c174d6bb8f47cb8
 
 
 let messageOffsetTrigger = 5; // maybe make command that can change this in client on the fly, so kept as let
@@ -61,87 +55,6 @@ let randomCatStuff = [
 client.on('messageCreate', async msg => {
 
 	// You can view the msg object here with console.log(msg)
-<<<<<<< HEAD
-	if (msg.author.username != 'Server Cat')  {
-		
-		// message content processing
-		let _channelid = msg.channelId;
-		if ( _channelid == "1282279204259627102" || _channelid == "1191181380604923954") {
-			let _messagecontent = msg.content;
-			let _lowercasemessage = _messagecontent.toLowerCase();
-			
-	
-			// random cat commands and such
-			if (messageOffsetTick >= messageOffsetTrigger) {
-	
-				messageOffsetTick = 0;
-	
-				console.log("reset messageOffsetIndex ", messageOffsetTick);
-	
-				client.channels.cache.get(_channelid).send(randomCatStuff[RandomInt(randomCatStuff.length)]);
-	
-			}
-	
-			messageOffsetTick++;
-			
-			console.log("set messageOffsetIndex ", messageOffsetTick);
-			console.log("ready for next message");
-			console.log();
-			
-			let catresponse = ServerCatMessageResponse(msg);
-	
-			if (catresponse != undefined) {
-				
-				client.channels.cache.get(_channelid).send(catresponse);
-	
-				console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+catresponse);
-			}
-
-			if (catresponse == undefined) {
-				client.channels.cache.get(_channelid).send("no responses");
-	
-				console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+"no responses");
-			}
-			
-	
-			// images
-			let _index = 0;
-			msg.attachments.forEach( attachment => {
-				_index++;
-				if (_index === 1 ) {
-					const url = attachment.url;
-				msg.channel.send({
-					content: url
-					});
-				console.log( url );
-				}
-				
-			});
-			_index = 0;
-	
-			// msg.attachments.forEach(attachment => {
-				
-			// 	});
-	
-			//client.channels.cache.get(_channelid).send(_msgattachments);
-	
-			// if (msg.attachments == undefined) {
-			// 	msg.reply('nothing here')
-			// 	.then(() => console.log(`Replied to message "${msg.content}"`))
-			// 	.catch(console.error);
-			// }
-	
-			// if (msg.attachments != undefined){
-			// 	msg.reply(msg.attachments)
-			// 	.then(() => console.log(`Replied to message "${msg.content}"`))
-			// 	.catch(console.error);
-			// }
-	
-			// logging
-			console.log(client.channels.cache.get(_channelid)+' | '+msg.author.globalName+': '+_messagecontent);
-		}
-
-=======
 	if (msg.author.username != 'Server Cat') {
 		
 		// message content processing
@@ -158,36 +71,15 @@ client.on('messageCreate', async msg => {
 			console.log("reset messageOffsetIndex ", messageOffsetTick);
 
 			client.channels.cache.get(_channelid).send(randomCatStuff[RandomInt(randomCatStuff.length)]);
-
 		}
 
 		messageOffsetTick++;
-		
+		client.channels.cache.get(_channelid).send(ServerCatMessageResponse(msg));
+		console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+ServerCatMessageResponse(msg));
+
 		console.log("set messageOffsetIndex ", messageOffsetTick);
 		console.log("ready for next message");
 		console.log();
-		
-		if (_lowercasemessage.includes(command1)) {
-			client.channels.cache.get(_channelid).send(response1);
-			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response1);
-		}
-
-		if (_lowercasemessage.includes(command2)){
-			client.channels.cache.get(_channelid).send(response2);
-			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response2);
-		}
-
-		if (_lowercasemessage.includes(command3)){
-			client.channels.cache.get(_channelid).send(response3);
-			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response3);
-		}
-
-		if (_lowercasemessage.includes(command4a) || _lowercasemessage.includes(command4b)){
-			client.channels.cache.get(_channelid).send(response4a);
-			client.channels.cache.get(_channelid).send(response4b);
-			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response4a);
-			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response4b);
-		}
 
 		// images
 		let _index = 0;
@@ -203,35 +95,10 @@ client.on('messageCreate', async msg => {
 			
 		});
 		_index = 0;
-		// msg.attachments.forEach(attachment => {
-			
-		// 	});
-		
-		
-
-		
-
-
-		//client.channels.cache.get(_channelid).send(_msgattachments);
-
-		
-		// if (msg.attachments == undefined) {
-		// 	msg.reply('nothing here')
-		// 	.then(() => console.log(`Replied to message "${msg.content}"`))
-		// 	.catch(console.error);
-		// }
-
-		// if (msg.attachments != undefined){
-		// 	msg.reply(msg.attachments)
-		// 	.then(() => console.log(`Replied to message "${msg.content}"`))
-		// 	.catch(console.error);
-		// }
-		
 		
 
 		// logging
 		console.log(client.channels.cache.get(_channelid)+' | '+msg.author.globalName+': '+_messagecontent);
->>>>>>> 1bea2f7dcb77a9662fddc05d7c174d6bb8f47cb8
 	}
 });
 
@@ -240,7 +107,6 @@ function RandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
-<<<<<<< HEAD
 function ServerCatMessageResponse(msg) {
 
 	var response;
@@ -273,6 +139,4 @@ function ServerCatMessageResponse(msg) {
 
 	return response;
 }
-=======
->>>>>>> 1bea2f7dcb77a9662fddc05d7c174d6bb8f47cb8
 // converting image to ascii
