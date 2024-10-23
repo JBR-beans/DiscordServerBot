e2ceca88bdd90e338220f39bd6f84fd8df973db6
JBRBeans <juliearitola1@gmail.com>
2024-10-23 11:10:30 -0700

    recieves images properly, still doesnt store

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Initialize dotenv
 require('dotenv').config();
+const fs = require('fs');
 
 // Discord.js versions ^13.0 require us to explicitly define client intents
 const { Client, GatewayIntentBits, Attachment, AttachmentBuilder } = require('discord.js');
@@ -26,17 +27,25 @@ const command3 = "food"; // contains
 const command4a = "laser";
 const command4b = "lazer";
 
+const command5 = ":3";
+
 const response1 = "*runs to you* meoooow :3";
 const response2 = "*Pukes on the carpet cutely* :3c mrrt"; // thank you seika
 const response3 = "*takes a bite when you're not looking*";
 
 const response4 = "*jumps around to catch it* n<(^w^)>n" + '\n' + "*prrt* :3c *catches light*";
-const response4a = "*jumps around to catch it* n<(^w^)>n";
-const response4b = "*prrt* :3c *catches light*";
+
+const response5 = ":3";
+
+//const response4a = "*jumps around to catch it* n<(^w^)>n";
+//const response4b = "*prrt* :3c *catches light*";
 
 
 let messageOffsetTrigger = 5; // maybe make command that can change this in client on the fly, so kept as let
 let messageOffsetTick = 0; // when this equals the trigger, send a random chat message event
+
+
+
 
 
 let randomCatStuff = [
@@ -62,8 +71,8 @@ client.on('messageCreate', async msg => {
 		let _messagecontent = msg.content;
 		let _lowercasemessage = _messagecontent.toLowerCase();
 		
-
-		// random cat commands and such
+		if (_channelid == "1282279204259627102" || _channelid == "1191181380604923954") {
+// random cat commands and such
 		if (messageOffsetTick >= messageOffsetTrigger) {
 
 			messageOffsetTick = 0;
@@ -74,8 +83,18 @@ client.on('messageCreate', async msg => {
 		}
 
 		messageOffsetTick++;
-		client.channels.cache.get(_channelid).send(ServerCatMessageResponse(msg));
-		console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+ServerCatMessageResponse(msg));
+
+		let response = ServerCatMessageResponse(msg); // returns the server cats response based on processed message content (msg)
+
+		if (response == undefined) {
+			response = "no response";
+		}
+		
+		if (response != undefined) {
+			client.channels.cache.get(_channelid).send(response);
+			console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response);
+		}
+		
 
 		console.log("set messageOffsetIndex ", messageOffsetTick);
 		console.log("ready for next message");
@@ -83,23 +102,30 @@ client.on('messageCreate', async msg => {
 
 		// images
 		let _index = 0;
-		msg.attachments.forEach( attachment => {
+		let msg_attachment = msg.attachments.forEach( attachment => {
 			_index++;
 			if (_index === 1 ) {
 				const url = attachment.url;
 			msg.channel.send({
 				content: url
 				});
+				
 			console.log( url );
 			}
 			
 		});
+		if (msg_attachment != undefined) {
+			ProcessImage(msg);
+		}
+		
 		_index = 0;
 		
 
 		// logging
 		console.log(client.channels.cache.get(_channelid)+' | '+msg.author.globalName+': '+_messagecontent);
 	}
+		}
+		
 });
 
 // utility
@@ -136,7 +162,22 @@ function ServerCatMessageResponse(msg) {
 		//client.channels.cache.get(_channelid).send(response4);
 		//console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response4);
 	}
+	if (_lowercasemessage.includes(command5)){
+		response = response5;
+		//client.channels.cache.get(_channelid).send(response4);
+		//console.log(client.channels.cache.get(_channelid)+' | '+"Server Cat"+': '+response4);
+	}
 
 	return response;
 }
 // converting image to ascii
+
+function ProcessImage(attachment) {
+	fs.writeFile("D:\CodeRepo\DiscordServerBot-main\DiscordServerBot-main\tmpimgs", attachment, err => {
+		if (err) {
+		  console.error(err);
+		} else {
+		  // file written successfully
+		}
+	  });
+}
